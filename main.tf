d520f444439bc15f2efb0721e448557bfa7694c3
Tejas-Murumkar <104541417+Tejas-Murumkar@users.noreply.github.com>
2024-06-13 00:30:09 +0530

    Update main.tf
    Created VPC and EC2 instance

--- main.tf
+++ main.tf
@@ -86,3 +86,125 @@ resource "aws_s3_bucket_acl" "name" {
 	bucket = aws_s3_bucket.bucket2.id
 	acl = "public-read"
 }
+
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/16"
+
+
+    tags = {
+        name = "aws_vpc",
+    } 
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true 
+
+  tags = {
+    name = "aws_subnet_1",
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+    tags = {
+      name = "aws_internet_gateway_1"
+    }
+  
+}
+
+
+resource "aws_route_table" "RT1" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    
+    }
+  
+}
+
+resource "aws_route_table_association" "association" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.RT1.id
+}
+# Launch the  on demand general purpose Instance 
+
+resource "aws_security_group" "ssh" {
+  name = "allow_Ssh"
+  description = "Allow SSH access"
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "inbound" {
+  security_group_id = aws_security_group.ssh.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "outbound" {
+  security_group_id = aws_security_group.ssh.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+resource "aws_network_interface" "network" {
+  subnet_id = aws_subnet.subnet1.id
+  private_ip = "10.0.1.100/24"
+}
+
+
+
+data "aws_key_pair" "key" {
+  key_name = "nat"
+  include_public_key = true 
+
+  filter {
+    name = "tag:nat_nv"
+    values = [ "N.virginia" ]
+  }
+}
+
+output "fingerprint" {
+  value = data.aws_key_pair.key.fingerprint
+}
+
+output "name" {
+  value = data.aws_key_pair.key.key_name
+}
+
+output "id" {
+  value = data.aws_key_pair.key.id
+}
+
+data "aws_ami" "ami" {
+    most_recent = true
+    owners = [ "amazon" ]
+    
+    filter {
+      name = "name"
+      values = [ "al2023-ami-2023.*-x86_64"]
+    }
+}
+
+resource "aws_network_interface_sg_attachment" "sg2" {
+  security_group_id = aws_security_group.ssh.id
+  network_interface_id = aws_instance.instace.primary_network_interface_id
+}
+resource "aws_instance" "instace" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+  key_name = "nat" 
+  network_interface {
+    network_interface_id = aws_network_interface.network.id
+    device_index = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
